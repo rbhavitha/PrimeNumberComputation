cfe14f098cfea895c3c05fc04d2ea202497de8fa
rbhavitha <bhavitharedrouthu@gmail.com>
2020-10-10 03:50:16 +0530

    Primes between 700 to 900 in reverse order

--- primeNumberComp.sh
+++ primeNumberComp.sh
@@ -8,27 +8,23 @@ checkPrime $((range))
 }
 
 function checkPrime () {
-count=1
-i=2
 range=$1
-while [ $count -le $range ]
+for (( i=700; i<=$(($1+1)); i++ ))
 do
 	flag=0
-	for (( j=2; j<=$(($i/2)); j++ ))
+	for (( j=2; j<$i; j++ ))
 	do
 		if [ $(($i%$j)) -eq 0 ]
 		then
 			flag=1
-			break
 		fi
 	done
 	if [ $flag -eq 0 ]
 	then
 		#echo $i
-		array[count]=$i
+		array[$count]=$i
 		((count++))
 	fi
-	((i++))
 done
 primesReverseOrder ${array[@]}
 }
@@ -37,7 +33,6 @@ function primesReverseOrder () {
 for (( i=${#array[@]}; i>=0; i-- ))
 do
 	echo "${array[i]}"
-	((i--))
 
 done
 }
