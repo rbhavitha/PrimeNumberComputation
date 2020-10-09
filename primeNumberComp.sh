b4f732c53905f6e7120166a52093fa35fa68c119
rbhavitha <bhavitharedrouthu@gmail.com>
2020-10-10 03:24:31 +0530

    first 100 primes in reverse order

--- primeNumberComp.sh
+++ primeNumberComp.sh
@@ -1,3 +1,44 @@
-#!/bin/bash
+#!/bin/bash 
 
 echo "Prime Number Computation"
+
+function initialize () {
+read -p " Enter range:" range
+checkPrime $((range))
+}
+
+function checkPrime () {
+count=1
+i=2
+range=$1
+while [ $count -le $range ]
+do
+	flag=0
+	for (( j=2; j<=$(($i/2)); j++ ))
+	do
+		if [ $(($i%$j)) -eq 0 ]
+		then
+			flag=1
+			break
+		fi
+	done
+	if [ $flag -eq 0 ]
+	then
+		#echo $i
+		array[count]=$i
+		((count++))
+	fi
+	((i++))
+done
+primesReverseOrder ${array[@]}
+}
+
+function primesReverseOrder () {
+for (( i=${#array[@]}; i>=0; i-- ))
+do
+	echo "${array[i]}"
+
+done
+}
+initialize
+
