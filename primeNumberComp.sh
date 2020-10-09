9ec88ca87e0da33b63a53154288780ea37aa8a75
rbhavitha <bhavitharedrouthu@gmail.com>
2020-10-10 04:12:17 +0530

    prime Palindromes between 1-200

--- primeNumberComp.sh
+++ primeNumberComp.sh
@@ -9,10 +9,10 @@ checkPrime $((range))
 
 function checkPrime () {
 range=$1
-for (( i=700; i<=$(($1+1)); i++ ))
+for (( i=2; i<=$(($1+1)); i++ ))
 do
 	flag=0
-	for (( j=2; j<$i; j++ ))
+	for (( j=2; j<(($i/2)); j++ ))
 	do
 		if [ $(($i%$j)) -eq 0 ]
 		then
@@ -26,7 +26,27 @@ do
 		((count++))
 	fi
 done
-primesReverseOrder ${array[@]}
+find_Palindrome ${array[@]}
+}
+function find_Palindrome ( ){
+for (( i=5; i<${#array[@]}; i++ ))
+do
+	number=${array[$i]}
+	rev=0
+	while [ $number -ne 0 ]
+	do
+        	rem=$(( $number%10 ))
+        	rev=$(( ($rev*10)+$rem ))
+        	number=$(( $number/10 ))
+	done
+
+	if [ $rev -eq ${array[i]} ]
+	then
+		echo $rev
+		primePalindrome[$i]=$rev	
+	fi
+	done
+echo "ARRAY OF primePalindromes:" ${primePalindrome[@]}
 }
 
 function primesReverseOrder () {
